b5556dc3b25e3b61e47c8abe61e48a71e91426b5
DerekBlackD <knljnr@gmail.com>
2024-04-13 01:55:44 -0500

    Review GoRoutines and Pointer

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -38,4 +39,27 @@ func main() {
 	for index, value := range s {
 		fmt.Println(index, value)
 	}
+
+	// Main solo tiene la funcion de crear la subrutina
+	// Pero no espera a que termine de ejecutar.
+	// Para eso crearemos los channels
+
+	c := make(chan int)
+	go doSomething(c)
+	<-c
+
+	// Apuntadores
+	g := 25
+	fmt.Println(g)
+	h := &g // Apuntamos a su ubicación física
+	fmt.Println(h)
+	fmt.Println(*h) // El asterisco transforma el valor de memoria en su valor
+}
+
+func doSomething(c chan int) {
+	time.Sleep(3 * time.Second)
+	fmt.Println("Done")
+	// La rutina main se bloqueará hasta que este canal
+	// Devuelva un mensaje
+	c <- 1
 }
