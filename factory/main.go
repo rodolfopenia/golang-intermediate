2a95ddae1972478adda3fe91c682121155666744
rodolfopenia <rodolfo.penia@hotmail.com>
2024-04-13 16:00:32 -0500

    Abastract Factory

--- factory/main.go
+++ factory/main.go
@@ -1,5 +1,90 @@
 package main
 
-func main() {
+import "fmt"
 
+// SMS , Email
+
+type INotificationFactory interface {
+	SendNotification()
+	GetSender() ISender
+}
+
+type ISender interface {
+	GetSenderMethod() string
+	GetSenderChannel() string
+}
+
+type SMSNotification struct {
+}
+
+func (SMSNotification) SendNotification() {
+	fmt.Println("Sending notificaciont via SMS")
+}
+
+func (SMSNotification) GetSender() ISender {
+	return SMSNotificationSender{}
+}
+
+type SMSNotificationSender struct {
+}
+
+func (SMSNotificationSender) GetSenderMethod() string {
+	return "SMS"
+}
+
+func (SMSNotificationSender) GetSenderChannel() string {
+	return "Twilio"
+}
+
+type EmailNotification struct {
+}
+
+func (EmailNotification) SendNotification() {
+	fmt.Println("Sending notificaciont via Email")
+}
+
+func (EmailNotification) GetSender() ISender {
+	return EmailNotificationSender{}
+}
+
+type EmailNotificationSender struct {
+}
+
+func (EmailNotificationSender) GetSenderMethod() string {
+	return "Email"
+}
+
+func (EmailNotificationSender) GetSenderChannel() string {
+	return "Hotmail"
+}
+
+func getNotificationFactory(notificationType string) (INotificationFactory, error) {
+	if notificationType == "SMS" {
+		return &SMSNotification{}, nil
+	}
+
+	if notificationType == "Email" {
+		return &EmailNotification{}, nil
+	}
+
+	return nil, fmt.Errorf("No notification type")
+}
+
+func sendNotification(f INotificationFactory) {
+	f.SendNotification()
+}
+
+func getMethod(f INotificationFactory) {
+	fmt.Println(f.GetSender().GetSenderMethod())
+}
+
+func main() {
+	smsFactory, _ := getNotificationFactory("SMS")
+	emailFactory, _ := getNotificationFactory("Email")
+
+	sendNotification(smsFactory)
+	sendNotification(emailFactory)
+
+	getMethod(smsFactory)
+	getMethod(emailFactory)
 }
