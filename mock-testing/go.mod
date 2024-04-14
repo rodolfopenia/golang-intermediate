cc0bb6c758762a1cbc3e11105e7cc11ba2d2d351
rodolfopenia <rodolfo.penia@hotmail.com>
2024-04-14 16:55:08 -0500

    mock testing

---
+++ mock-testing/go.mod
@@ -0,0 +1,3 @@
+module github.com/rodolfopenia/mock-testin
+
+go 1.18
---
+++ mock-testing/main_test.go
@@ -0,0 +1,61 @@
+package main
+
+import "testing"
+
+func TestGetFullTimeEmployeeById(t *testing.T) {
+	table := []struct {
+		id               int
+		dni              string
+		mockFunc         func()
+		expectedEmployee FullTimeEmployee
+	}{
+		{
+			id:  1,
+			dni: "1",
+			mockFunc: func() {
+				GetEmployeeById = func(id int) (Employee, error) {
+					return Employee{
+						Id:       1,
+						Position: "CEO",
+					}, nil
+				}
+
+				GetPersonByDni = func(id string) (Person, error) {
+					return Person{
+						Name: "John Doe",
+						Age:  35,
+						DNI:  "1",
+					}, nil
+				}
+			},
+			expectedEmployee: FullTimeEmployee{
+				Person: Person{
+					Age:  35,
+					DNI:  "1",
+					Name: "John Doe",
+				},
+				Employee: Employee{
+					Id:       1,
+					Position: "CEO",
+				},
+			},
+		},
+	}
+	originalGetEmployeeById := GetEmployeeById
+	originalGetPersonByDni := GetPersonByDni
+	for _, test := range table {
+		test.mockFunc()
+		ft, err := GetFullTimeEmployeeById(test.id, test.dni)
+		if err != nil {
+			t.Errorf("Error when getting Employee")
+		}
+
+		if ft.Age != test.expectedEmployee.Age {
+			t.Errorf("Error, got %d expected %d", ft.Age, test.expectedEmployee.Age)
+		}
+	}
+
+	GetEmployeeById = originalGetEmployeeById
+	GetPersonByDni = originalGetPersonByDni
+
+}
